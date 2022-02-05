e7e844846a132fff0d5fce7fc0d964cc1a352e52
Sandip Sadhukhan <sandippakhanna@gmail.com>
2022-02-05 10:29:54 +0530

    custom user register in admin with form

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import UserAccount
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+class UserAccountAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = UserAccount
+    list_display = (
+        "name",
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "name",
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("name", "email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "name",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+    search_fields = (
+        "name",
+        "email",
+    )
+    ordering = ("name",)
+
+
+admin.site.register(UserAccount, UserAccountAdmin)
